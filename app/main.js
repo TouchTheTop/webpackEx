3312739d75a848e8b5277db04895a2d85d3d8c2d
Christine <1204356932@qq.com>
2017-02-10 08:56:53 +0800

    Firsr Commit

--- app/main.js
+++ app/main.js
@@ -1,6 +1,7 @@
 
 import greeter from './Greeter.js';
-import React from 'react';
+import React from 'react'; // React 0.14版本之后将react package拆分成了 react 和 react-dom 两个package
+import ReactDom from 'react-dom';
 
 let AppComponent = require('./components/productBox.jsx');
 let Running=greeter.Running;
@@ -9,8 +10,8 @@ require(["jquery"], function ($) {
  $('#root').append(greeter());
 console.log(greeter.tag);
 console.log(Running.a);
-console.log(Running.findMaxDuplicateChar());
-React.render(
+console.log(Running.findMaxDuplicateChar('dffd'));
+ReactDom.render(
 	<AppComponent />, document.getElementById('root')
 	);
 });
